3de9da12d9f2780740e6735c5097c304342aeb36
Tohyo <kevin@codekraft.fr>
2021-05-08 12:54:56 +0200

    Feat: Add user classes

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -6,4 +6,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
