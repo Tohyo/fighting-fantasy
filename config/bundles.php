cdb4c4043f9bb8c1ef9cb78c66f119e96799ea71
Tohyo <k.baujard@orange.fr>
2023-05-14 12:16:57 +0200

    add tailwind config

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
