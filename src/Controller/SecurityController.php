8ffa27ceafbd1b48262169f7229c611d8a9a038f
Tohyo <k.baujard@orange.fr>
2023-05-22 21:26:54 +0200

    deny access to admin and save last email used

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,6 +15,7 @@ class SecurityController extends AbstractController
     {
         return $this->render('security/login.html.twig', [
             'error' => $authenticationUtils->getLastAuthenticationError(),
+            'last_username' => $authenticationUtils->getLastUsername(),
         ]);
     }
 
