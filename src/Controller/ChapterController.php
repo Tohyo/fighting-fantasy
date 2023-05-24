29ff0a6f8eccb7df2db1d2f2bc31bba998b562ad
Tohyo <k.baujard@orange.fr>
2023-05-24 19:02:56 +0200

    save user chapter progression

--- src/Controller/ChapterController.php
+++ src/Controller/ChapterController.php
@@ -3,15 +3,26 @@
 namespace App\Controller;
 
 use App\Entity\Chapter;
+use App\Repository\AdventureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ChapterController extends AbstractController
 {
     #[Route('/chapter/{id}', name: 'app_chapter')]
-    public function getChapter(Chapter $chapter): Response
-    {
+    #[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
+    public function getChapter(
+        Chapter $chapter,
+        AdventureRepository $adventureRepository,
+    ): Response {
+        $adventure = $adventureRepository->findOneBy([
+            'player' => $this->getUser(),
+            'book' => $chapter->getBook(),
+        ])->setChapter($chapter);
+        $adventureRepository->save($adventure, true);
+
         return $this->render('chapter/chapter.html.twig', [
             'chapter' => $chapter,
         ]);
