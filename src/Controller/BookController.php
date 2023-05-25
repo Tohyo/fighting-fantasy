88828f7bceec654305f5e21a2e5224767a6a6984
Tohyo <k.baujard@orange.fr>
2023-05-25 21:29:04 +0200

    add book voter to block edit if user is not the book creator

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,16 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Security\Voter\BookVoter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AppAbstractController
 {
-    #[Route('/book/{slug}', name: 'app_book')]
-    public function getBook(Book $book): Response
+    #[Route('/book/{id}', requirements: ['id' => '\d+'], name: 'app_book_edit')]
+    public function edit(Book $book): Response
     {
-        return $this->render('book/book.html.twig', [
+        $this->denyAccessUnlessGranted(BookVoter::EDIT, $book);
+
+        return $this->render('book/edit.html.twig', [
+            'book' => $book
+        ]);
+    }
+
+    #[Route('/book/{slug}', name: 'app_book')]
+    public function show(Book $book): Response
+    {
+        return $this->render('book/show.html.twig', [
             'book' => $book,
         ]);
     }
+
 }
---
+++ src/Security/Voter/BookVoter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Book;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class BookVoter extends Voter
+{
+    public const EDIT = 'POST_EDIT';
+    public const VIEW = 'POST_VIEW';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::EDIT, self::VIEW])
+            && $subject instanceof \App\Entity\Book;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if (!$subject instanceof Book) {
+            return false;
+        }
+
+        if ($user === $subject->getCreator()) {
+            return true;
+        }
+
+        return false;
+    }
+}
