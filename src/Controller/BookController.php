b11ddcced195ac05cd60bcd5c0fdb67b330bfc86
Tohyo <k.baujard@orange.fr>
2023-05-25 21:41:37 +0200

    edit book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,19 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Form\BookType;
+use App\Repository\BookRepository;
 use App\Security\Voter\BookVoter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AppAbstractController
 {
     #[Route('/book/{id}', requirements: ['id' => '\d+'], name: 'app_book_edit')]
-    public function edit(Book $book): Response
+    public function edit(Request $request, Book $book, BookRepository $bookRepository): Response
     {
         $this->denyAccessUnlessGranted(BookVoter::EDIT, $book);
 
+        $form = $this->createForm(BookType::class, $book);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bookRepository->save($book, true);
+
+            return $this->redirectToRoute('app_book', ['slug' => $book->getSlug()]);
+        }
+
         return $this->render('book/edit.html.twig', [
-            'book' => $book
+            'book' => $book,
+            'form' => $form
         ]);
     }
 
