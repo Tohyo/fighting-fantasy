178fc95bc416d264da7bfd26f73b1c33da4b19eb
Tohyo <k.baujard@orange.fr>
2023-05-25 16:11:19 +0200

    add adventures and books profile page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -14,6 +15,17 @@ class ProfileController extends AppAbstractController
     {
         return $this->render('profile/index.html.twig', [
             'adventures' => $this->getUser()->getAdventures(),
+        ]);
+    }
+
+    #[IsGranted("IS_AUTHENTICATED_REMEMBERED")]
+    #[Route('/profile/books', name: 'app_books_profile')]
+    public function getUserBooks(BookRepository $bookRepository): Response
+    {
+        $books = $bookRepository->findBy(['creator' => $this->getUser()]);
+
+        return $this->render('profile/books.html.twig', [
+            'books' => $books
         ]);
     }
 }
