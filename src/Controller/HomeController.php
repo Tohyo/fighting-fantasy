4bdc671e37e87acb26ff1eca2bc418fe9b640205
Tohyo <k.baujard@orange.fr>
2023-05-25 20:45:31 +0200

    remove book list route

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AppAbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'books' => $bookRepository->findAll(),
+        ]);
     }
 }
