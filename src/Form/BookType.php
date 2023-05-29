fc6e27f0fe75b5d21c3b1273a311c9c25604e115
Tohyo <k.baujard@orange.fr>
2023-05-29 14:15:13 +0200

    add css to book form

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,7 @@ class BookType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('description')
-            ->add('save', SubmitType::class)
+            ->add('description', TextareaType::class)
         ;
     }
 
